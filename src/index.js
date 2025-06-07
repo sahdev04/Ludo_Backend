bcc02be98a43fea73d44d86b7b550682f148c2e7
trapti patel <traptipatel46@gmail.com>
2025-06-07 15:18:34 +0530

    setup for deploy

--- src/index.js
+++ src/index.js
@@ -7,22 +7,20 @@ import { initializeSocket } from "./config/socket.js";
 dotenv.config({ path: "./.env" });
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 8000;
-
 // Attach WebSocket to server
 const io = initializeSocket(server);
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log("Database connected successfully!");
+    // console.log("Database connected successfully!");
 
     await sequelize.sync();
-    console.log("Database synced!");
+    //console.log("Database synced!");
 
     // Start server only after DB is ready
-    server.listen(PORT, () => {
-      console.log(` Server running on port ${PORT}`);
+    server.listen(process.env.PORT, () => {
+      console.log(` Server running on port ${process.env.PORT}`);
     });
   } catch (err) {
     console.error("Database connection error:", err);
