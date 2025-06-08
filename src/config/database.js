aba11bcb17dd8af540ce81e1e43cfe72ba9a3c39
trapti patel <traptipatel46@gmail.com>
2025-06-08 16:17:20 +0530

    .env changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
+dotenv.config({ path: "./.env" });
 // Initialize Sequelize connection
-const sequelize = new Sequelize("ludogame", "postgres", "root", {
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 //  Correct way to test the connection
 const testDbConnection = async () => {
